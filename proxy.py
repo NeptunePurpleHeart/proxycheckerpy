9877cf37ceab44850cb76429b2cd2e2c950fc2a7
NeptunePurpleHeart <54174780+NeptunePurpleHeart@users.noreply.github.com>
2021-01-10 00:21:23 +0200

    Some style changes | add if __name__..

--- proxy.py
+++ proxy.py
@@ -1,13 +1,16 @@
 import requests
-import urllib.request
 import speedtest
 import asyncio
 import socks
+
 s = socks.socksocket()
+
 print("Site to check")
+
 check_site = input()
 input_data = []
 working = {'proxy':[], 'speed':[]}
+
 async def proxy_speed(proxy):
     speed = speedtest.Speedtest()
     speed.get_best_server()
@@ -20,6 +23,7 @@ async def proxy_speed(proxy):
     working['speed'].append(f"upload:{uplo:.2f} mbit/s, download:{down:.2f} mbit/s, ping:{results_dict['ping']} ms")
     s.close()
     return results_dict
+
 async def check(proxy):
     try:
         requests.get(check_site, proxies={'http':'http://'+proxy})
@@ -31,16 +35,20 @@ async def check(proxy):
         s.set_proxy(socks.HTTP, prox, port)
         working['proxy'].append(proxy)
         return True
-with open("proxy_list.txt", "r") as proxy_list:
-    for line in proxy_list:
-        input_data.append(line.strip())
-    for i in input_data:
-        r = asyncio.run(check(i))
-        if r == True:
-            asyncio.run(proxy_speed(i))
-        print(i + " " + "checked")
-    with open("result.txt", "w") as result:
-        result.write(f"{check_site}\n")
-        for i in range(len(working['proxy'])):
-            result.write(f"{working['proxy'][i]}: {working['speed'][i]}\n")
-print("Done")
+async def main():
+    with open("proxy_list.txt", "r") as proxy_list:
+        for line in proxy_list:
+            input_data.append(line.strip())
+        for i in input_data:
+            r = await check(i)
+            if r == True:
+                asyncio.run(proxy_speed(i))
+            print(i + " " + "checked")
+        with open("result.txt", "w") as result:
+            result.write(f"{check_site}\n")
+            for i in range(len(working['proxy'])):
+                result.write(f"{working['proxy'][i]}: {working['speed'][i]}\n")
+    print("Done")
+
+if __name__ == "__main__":
+    await main()
